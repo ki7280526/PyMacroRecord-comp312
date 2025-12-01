2ac11675e9b78335661aa3eeb023a29e00eb3382
Jennifer <jnambo@luc.edu>
2025-12-01 01:44:12 -0600

    Added support for light/dark theme and tested switching ability

--- PyMacroRecord-main/src/utils/theme_style.py
+++ PyMacroRecord-main/src/utils/theme_style.py
@@ -1,18 +1,18 @@
 class ThemeStyle: #styles for light/dark mode (taken from color-hex.com) 
-  THEMES = {
-    "Light": {
+    THEMES = {
+      "Light": {
       "bg": "#fafafa",
       "fg": "#1e1e1e",
       "button_bg": "#e6e6e6",
       "button_fg": "#1e1e1e"
-    },
-    "Dark": {
-      "bg": "#1e1e1e",
-      "fg": "#f2f2f2", 
-      "button_bg": "#2d2d2d",
-      "button_fg": "#f2f2f2"
-    }
-}
+      },
+      "Dark": {
+        "bg": "#1e1e1e",
+        "fg": "#f2f2f2", 
+        "button_bg": "#2d2d2d",
+        "button_fg": "#f2f2f2"
+      }
+  }
 
 def get(theme_option: str): 
   return ThemeStyle.THEMES.get(theme_option, ThemeStyle.THEMES["Light"])
